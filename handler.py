9604f9a0eb0daf719aea2065d24ff67f3d311a94
Isaac Solis Ramirez <ing_isaacsolis@hotmail.com>
2024-01-08 07:27:31 -0700

    1.0.0.4

--- handler.py
+++ handler.py
@@ -5,15 +5,22 @@ import runpod
 from llama_cpp import Llama
 
 #TEST
-#os.environ["LLAMA_ARGS"] = '{"model_path": "./models/openhermes-2.5-neural-chat-7b-v3-1-7b.Q4_K_M.gguf", "n_gpu_layers": -1, "n_ctx": 28000, "max_tokens": 8000, "temperature": 0.2, "stream": "true"}'
+#os.environ["LLAMA_ARGS"] = '{"model_path": "./models/openhermes-2.5-neural-chat-7b-v3-1-7b.Q4_K_M.gguf", "n_gpu_layers": -1, "n_ctx": 28000, "temperature": 0.2}'
 
 args = json.loads(os.environ.get("LLAMA_ARGS", "{}"))
 llm = Llama(**args)
 
 def handler(event):
-    inp = event["input"]    
-    return llm(**inp)
-
+    inp = event["input"]  
+    stream = inp['stream']
+    
+    if stream:  
+        response = llm.create_completion(**inp)
+        for chunk in response:
+            yield chunk["choices"][0]["text"]
+    else:
+        return llm.create_completion(**inp)
+             
 runpod.serverless.start({
     "handler": handler,
     "return_aggregate_stream": True
